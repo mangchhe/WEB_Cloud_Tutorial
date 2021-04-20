4e06013f3844ae88a79a4f8c183130792e840613
HaJooHyun <mangchhe@naver.com>
2021-04-20 12:06:25 +0900

    Add User MicroService [사용자 조회 구현]

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -13,13 +13,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("user-service")
 public class UserController {
 
     private final Environment env;
@@ -31,7 +32,8 @@ public class UserController {
 
     @GetMapping("health_check")
     public String status(){
-        return "It's working User Service.";
+        return String.format("It's Working in User Service on PORT %s",
+                env.getProperty("local.server.port"));
     }
 
     @GetMapping("welcome")
@@ -46,5 +48,26 @@ public class UserController {
         userService.createUser(userDto);
         ResponseUser responseUser = modelMapper.map(userDto, ResponseUser.class);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<ResponseUser>> getUsers(){
+        Iterable<UserEntity> userList = userService.getUserByAll();
+
+        List<ResponseUser> result = new ArrayList<>();
+        userList.forEach(v ->
+                result.add(modelMapper.map(v, ResponseUser.class)));
+
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<ResponseUser> getUser(@PathVariable("userId") String userId){
+        UserDto userDto = userService.getUserByUserId(userId);
+
+        ResponseUser result = modelMapper.map(userDto, ResponseUser.class);
+
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+
     }
 }
--- user-service/src/main/java/com/example/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/example/userservice/repository/UserRepository.java
@@ -4,4 +4,7 @@ import com.example.userservice.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    UserEntity findByUserId(String userId);
 }
+
