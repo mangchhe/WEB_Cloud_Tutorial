0fc6701c89fa8bb3447e8e7d208090c07ce4d002
HaJooHyun <mangchhe@naver.com>
2021-04-20 14:58:11 +0900

    Add Catalog MicroService [상품 목록 조회 구현]

---
+++ catalog-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-001', 'Banana', 100, 1500);
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-002', 'Apple', 700, 1000);
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-003', 'Orange', 300, 3000);
