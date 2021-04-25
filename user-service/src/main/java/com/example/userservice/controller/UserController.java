8c3dbf72a52fac100ef616fa681db9327ebc33ff
HaJooHyun <mangchhe@naver.com>
2021-04-25 23:49:14 +0900

    Spring Cloud Config [User Service 연동]

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -32,8 +32,11 @@ public class UserController {
 
     @GetMapping("health_check")
     public String status(){
-        return String.format("It's Working in User Service on PORT %s",
-                env.getProperty("local.server.port"));
+        return String.format("It's Working in User Service"
+                + ", local port : " + env.getProperty("local.server.port")
+                + ", port : " + env.getProperty("server.port")
+                + ", secret : " + env.getProperty("token.secret")
+                + ", expiration_time : " + env.getProperty("token.expiration_time"));
     }
 
     @GetMapping("welcome")
