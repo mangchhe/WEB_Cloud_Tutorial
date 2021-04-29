1c0e23149e93af33cd52c555ecd4e80b0b9d91a0
HaJooHyun <mangchhe@naver.com>
2021-04-29 15:20:49 +0900

    Update User Service [RestTemplate를 이용하여 OrderService에 사용자 주문 목록 가져오기 구현]

--- user-service/src/main/java/com/example/userservice/config/ApplicationConfig.java
+++ user-service/src/main/java/com/example/userservice/config/ApplicationConfig.java
@@ -2,9 +2,11 @@ package com.example.userservice.config;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ApplicationConfig {
@@ -20,6 +22,12 @@ public class ApplicationConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 
 }
--- user-service/src/main/java/com/example/userservice/vo/ResponseOrder.java
+++ user-service/src/main/java/com/example/userservice/vo/ResponseOrder.java
@@ -1,7 +1,11 @@
 package com.example.userservice.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter @Setter
 public class ResponseOrder {
     private String productId;
     private Integer qty;
