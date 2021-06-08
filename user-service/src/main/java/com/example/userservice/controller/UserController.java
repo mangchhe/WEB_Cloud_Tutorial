e79534eb1ce28a253ccbf09c838b8f01a3978ea3
HaJooHyun <mangchhe@naver.com>
2021-06-08 23:44:02 +0900

    Update Apigateway, Order, User Service [Micrometer 모니터링 시스템 구현]

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -6,6 +6,7 @@ import com.example.userservice.service.UserService;
 import com.example.userservice.vo.Greeting;
 import com.example.userservice.vo.RequestUser;
 import com.example.userservice.vo.ResponseUser;
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
@@ -31,6 +32,7 @@ public class UserController {
     private Greeting greeting;
 
     @GetMapping("health_check")
+    @Timed(value = "users.status", longTask = true)
     public String status(){
         return String.format("It's Working in User Service"
                 + ", local port : " + env.getProperty("local.server.port")
@@ -40,6 +42,7 @@ public class UserController {
     }
 
     @GetMapping("welcome")
+    @Timed(value = "users.welcome", longTask = true)
     public String welcome(){
         return greeting.getMessage();
 //        return env.getProperty("greeting.message");
